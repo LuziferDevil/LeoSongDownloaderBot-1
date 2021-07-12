0d3ca425e586675a6308593467e1bd7f99315fb9
Navindu Dilmin <74837844+Naviya2@users.noreply.github.com>
2021-07-12 18:02:49 +0530

    Update broadcast.py

--- helper/broadcast.py
+++ helper/broadcast.py
@@ -15,9 +15,9 @@ broadcast_ids = {}
 
 async def send_msg(user_id, message):
     try:
-        if Config.BROADCAST_AS_COPY is False:
+        if config.BROADCAST_AS_COPY is False:
             await message.forward(chat_id=user_id)
-        elif Config.BROADCAST_AS_COPY is True:
+        elif config.BROADCAST_AS_COPY is True:
             await message.copy(chat_id=user_id)
         return 200, None
     except FloodWait as e:

b4fcf830188961110dbe1f45751e43cdfd7a3e46
Navindu Dilmin <74837844+Naviya2@users.noreply.github.com>
2021-07-12 23:30:48 +0530

    Update songs.py

--- LeoSongDownloaderBot/plugins/songs.py
+++ LeoSongDownloaderBot/plugins/songs.py
@@ -44,17 +44,17 @@ def song(client, message):
         yt = YouTube(link)
     except Exception as e:
         m.edit(
-            "❌ Nothing Found.\n\nTry another keyword or maybe spell it properly."
+            "❌ Nothing Found.\n\nTry another keyword or maybe spell it properly 😊"
         )
         print(str(e))
         return
-    m.edit("Downloading...")
+    m.edit("Now I am Downloading Your Song 😊\n\nleosongdownloaderbot 🇱🇰")
     try:
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = 'Downloaded By @leosongdownloaderbot 🇱🇰\nJoin Our Updates Channel : @new_ehi 🇱🇰\nJoin Our Support Group : @leosupportx 🇱🇰'
+        rep = 'Downloaded By : @leosongdownloaderbot 🇱🇰'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
@@ -62,7 +62,7 @@ def song(client, message):
         s = message.reply_audio(audio_file, caption=rep, thumb=thumb_name, parse_mode='md', title=title, duration=dur, performer=str(yt.author))
         m.delete()
     except Exception as e:
-        m.edit('❌ Error')
+        m.edit('❌ An error occured 🙁 please ask in our [Support Group](https://t.me/leosupportx)')
         print(e)
 
     try:

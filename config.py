b29ecec8b5201872c9b4fdaaea3338813306fba9
Navindu Dilmin <74837844+Naviya2@users.noreply.github.com>
2021-07-12 13:34:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,4 +5,7 @@ API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", None)
-
+BOT_USERNAME = os.environ.get("BOT_USERNAME")
+SESSION_NAME = os.environ.get("SESSION_NAME", "LeoSongDownloaderBot")
+BOT_OWNER = int(os.environ.get("BOT_OWNER", 1069002447))
+BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))

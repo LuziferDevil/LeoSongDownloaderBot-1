ad7344b1f950923fcc759ed95428cacaad1d05f8
Navindu Dilmin <74837844+Naviya2@users.noreply.github.com>
2021-07-28 16:38:30 +0530

    Update cb_buttons.py

--- LeoSongDownloaderBot/plugins/cb_buttons.py
+++ LeoSongDownloaderBot/plugins/cb_buttons.py
@@ -3,29 +3,29 @@ from LeoSongDownloaderBot.translation import Translation
 import config
 from pyrogram import Client
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-
+from LeoSongDownloaderBot import LeoSongDownloaderBot as app
 
 @app.on_callback_query()
 async def cb_data(client, message):
-    if message.data = "home":
+    if message.data == "home":
         await message.message.edit_text(
             text=Translation.START_TEXT.format(message.from_user.mention),
             reply_markup=Translation.START_BUTTONS,
             disable_web_page_preview=True
         )
-    elif message.data = "help":
+    elif message.data == "help":
         await message.message.edit_text(
             text=Translation.HELP_TEXT.format(message.from_user.mention),
             reply_markup=Translation.HELP_BUTTONS,
             disable_web_page_preview=True
         )
-    elif message.data = "about":
+    elif message.data == "about":
         await message.message.edit_text(
             text=Translation.ABOUT_TEXT,
             reply_markup=Translation.ABOUT_BUTTONS,
             disable_web_page_preview=True
         )
-    elif message.data = "info":
+    elif message.data == "info":
         await message.message.edit_text(
             text=Translation.INFO_TEXT.format(username=message.from_user.username, first_name=message.from_user.first_name, last_name=message.from_user.last_name, user_id=message.from_user.id, mention=message.from_user.mention),
             reply_markup=Translation.INFO_BUTTONS,

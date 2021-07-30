95aac6d5abc7698a9cafb009eb257dc66e0793de
Navindu Dilmin <74837844+Naviya2@users.noreply.github.com>
2021-07-31 00:54:23 +0530

    Update cb_buttons.py

--- LeoSongDownloaderBot/plugins/cb_buttons.py
+++ LeoSongDownloaderBot/plugins/cb_buttons.py
@@ -1,6 +1,7 @@
 import os
 from LeoSongDownloaderBot.translation import Translation
 import config
+from LeoSongDownloaderBot.__main__ import LEOSONGDLIMG
 from pyrogram import Client
 from pyrogram.errors import UserNotParticipant
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
@@ -9,10 +10,10 @@ from LeoSongDownloaderBot import LeoSongDownloaderBot as app
 @app.on_callback_query()
 async def cb_data(client, message):
     if message.data == "home":
-        await message.message.edit_text(
-            text=Translation.START_TEXT.format(message.from_user.mention),
+        await message.message.edit_photo(
+            LEOSONGDLIMG,
+            caption=Translation.START_TEXT.format(message.from_user.mention),
             reply_markup=Translation.START_BUTTONS,
-            disable_web_page_preview=True
         )
     elif message.data == "help":
         await message.message.edit_text(

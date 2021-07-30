42403464b7360e32465ddb7fba4653133cbbaed0
Navindu Dilmin <74837844+Naviya2@users.noreply.github.com>
2021-07-30 22:54:09 +0530

    Update songs.py

--- LeoSongDownloaderBot/plugins/songs.py
+++ LeoSongDownloaderBot/plugins/songs.py
@@ -27,209 +27,61 @@ aiohttpsession = aiohttp.ClientSession()
 
 arq = ARQ("https://thearq.tech", ARQ_API_KEY, aiohttpsession)
 
-@app.on_message(filters.command("song") & ~filters.channel)
+@app.on_message(filters.command(['yts', f'yts@{BOT_USERNAME}']))
 def song(client, message):
 
-    user_id = message.from_user.id
-    user_name = message.from_user.first_name
-    rpk = "[" + user_name + "](tg://user?id=" + str(user_id) + ")"
+    user_id = message.from_user.id 
+    user_name = message.from_user.first_name 
+    rpk = "["+user_name+"](tg://user?id="+str(user_id)+")"
 
-    query = ""
+    query = ''
     for i in message.command[1:]:
-        query += " " + str(i)
+        query += ' ' + str(i)
     print(query)
-    m = message.reply("Now I am Searching Your Song 🔎\n\n@leosongdownloaderbot 🇱🇰")
+    m = message.reply('**Please Wait! Im Searching For Your Song 🔎...**')
     ydl_opts = {"format": "bestaudio[ext=m4a]"}
     try:
         results = YoutubeSearch(query, max_results=1).to_dict()
         link = f"https://youtube.com{results[0]['url_suffix']}"
-        # print(results)
-        title = results[0]["title"][:40]
+        #print(results)
+        title = results[0]["title"][:40]       
         thumbnail = results[0]["thumbnails"][0]
-        thumb_name = f"thumb{title}.jpg"
+        thumb_name = f'thumb{title}.jpg'
         thumb = requests.get(thumbnail, allow_redirects=True)
-        open(thumb_name, "wb").write(thumb.content)
+        open(thumb_name, 'wb').write(thumb.content)
+
 
         duration = results[0]["duration"]
-        results[0]["url_suffix"]
-        results[0]["views"]
+        url_suffix = results[0]["url_suffix"]
+        views = results[0]["views"]
 
     except Exception as e:
-        m.edit("Nothing Found {} ☹️\n\nPlease check your spellings and try again😊".format(message.from_user.mention))
+        m.edit(
+            "Sorry To Say but I can't find anything ❌!\n\nTry Another Keyword! Btw you spelled it properly 🤔?"
+        )
         print(str(e))
         return
-    m.edit("Now I am Downloading Your Song ⏳\n\nPlease Wait 😊")
+    m.edit("**Downloading Your Song! Please Wait ⏰**")
     try:
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = "Downloaded By : @leosongdownloaderbot 🇱🇰\n\nRequested By : {} 😊".format(message.from_user.mention)
-        secmul, dur, dur_arr = 1, 0, duration.split(":")
-        for i in range(len(dur_arr) - 1, -1, -1):
-            dur += int(dur_arr[i]) * secmul
+        rep = f'🎙 **Title**: [{title[:35]}]({link})\n🎬 **Source**: `YouTube`\n⏱️ **Song Duration**: `{duration}`\n👁‍🗨 **Song Views**: `{views}`\n\n**Uploaded By**: **@{BOT_USERNAME}** \n **Join @{UPDATES_CHANNEL} 😉** '
+        secmul, dur, dur_arr = 1, 0, duration.split(':')
+        for i in range(len(dur_arr)-1, -1, -1):
+            dur += (int(dur_arr[i]) * secmul)
             secmul *= 60
-        message.reply_audio(
-            audio_file,
-            caption=rep,
-            thumb=thumb_name,
-            parse_mode="HTML",
-            title=title,
-            duration=dur,
-        )
+        message.reply_audio(audio_file, caption=rep, thumb=thumb_name, parse_mode='md', title=title, duration=dur)
         m.delete()
     except Exception as e:
-        m.edit("An error occured {} ☹️ Please feel free to say in our support group 😊 \n\nSupport Group : @leosupportx  🇱🇰".format(message.from_user.mention))
-        print(e)
+        m.edit(e)
 
     try:
         os.remove(audio_file)
         os.remove(thumb_name)
     except Exception as e:
         print(e)
-
-
-def get_text(message: Message) -> [None, str]:
-    text_to_return = message.text
-    if message.text is None:
-        return None
-    if " " in text_to_return:
-        try:
-            return message.text.split(None, 1)[1]
-        except IndexError:
-            return None
-    else:
-        return None
-
-
-def humanbytes(size):
-    if not size:
-        return ""
-    power = 2 ** 10
-    raised_to_pow = 0
-    dict_power_n = {0: "", 1: "Ki", 2: "Mi", 3: "Gi", 4: "Ti"}
-    while size > power:
-        size /= power
-        raised_to_pow += 1
-    return str(round(size, 2)) + " " + dict_power_n[raised_to_pow] + "B"
-
-
-async def progress(current, total, message, start, type_of_ps, file_name=None):
-    now = time.time()
-    diff = now - start
-    if round(diff % 10.00) == 0 or current == total:
-        percentage = current * 100 / total
-        speed = current / diff
-        elapsed_time = round(diff) * 1000
-        if elapsed_time == 0:
-            return
-        time_to_completion = round((total - current) / speed) * 1000
-        estimated_total_time = elapsed_time + time_to_completion
-        progress_str = "{0}{1} {2}%\n".format(
-            "".join(["🔴" for i in range(math.floor(percentage / 10))]),
-            "".join(["🔘" for i in range(10 - math.floor(percentage / 10))]),
-            round(percentage, 2),
-        )
-        tmp = progress_str + "{0} of {1}\nETA: {2}".format(
-            humanbytes(current), humanbytes(total), time_formatter(estimated_total_time)
-        )
-        if file_name:
-            try:
-                await message.edit(
-                    "{}\n**File Name:** `{}`\n{}".format(type_of_ps, file_name, tmp)
-                )
-            except FloodWait as e:
-                await asyncio.sleep(e.x)
-            except MessageNotModified:
-                pass
-        else:
-            try:
-                await message.edit("{}\n{}".format(type_of_ps, tmp))
-            except FloodWait as e:
-                await asyncio.sleep(e.x)
-            except MessageNotModified:
-                pass
-
-
-def get_user(message: Message, text: str) -> [int, str, None]:
-    if text is None:
-        asplit = None
-    else:
-        asplit = text.split(" ", 1)
-    user_s = None
-    reason_ = None
-    if message.reply_to_message:
-        user_s = message.reply_to_message.from_user.id
-        reason_ = text if text else None
-    elif asplit is None:
-        return None, None
-    elif len(asplit[0]) > 0:
-        user_s = int(asplit[0]) if asplit[0].isdigit() else asplit[0]
-        if len(asplit) == 2:
-            reason_ = asplit[1]
-    return user_s, reason_
-
-
-def get_readable_time(seconds: int) -> int:
-    count = 0
-    ping_time = ""
-    time_list = []
-    time_suffix_list = ["s", "m", "h", "days"]
-
-    while count < 4:
-        count += 1
-        if count < 3:
-            remainder, result = divmod(seconds, 60)
-        else:
-            remainder, result = divmod(seconds, 24)
-        if seconds == 0 and remainder == 0:
-            break
-        time_list.append(int(result))
-        seconds = int(remainder)
-
-    for x in range(len(time_list)):
-        time_list[x] = str(time_list[x]) + time_suffix_list[x]
-    if len(time_list) == 4:
-        ping_time += time_list.pop() + ", "
-
-    time_list.reverse()
-    ping_time += ":".join(time_list)
-
-    return ping_time
-
-
-def time_formatter(milliseconds: int) -> str:
-    seconds, milliseconds = divmod(int(milliseconds), 1000)
-    minutes, seconds = divmod(seconds, 60)
-    hours, minutes = divmod(minutes, 60)
-    days, hours = divmod(hours, 24)
-    tmp = (
-        ((str(days) + " day(s), ") if days else "")
-        + ((str(hours) + " hour(s), ") if hours else "")
-        + ((str(minutes) + " minute(s), ") if minutes else "")
-        + ((str(seconds) + " second(s), ") if seconds else "")
-        + ((str(milliseconds) + " millisecond(s), ") if milliseconds else "")
-    )
-    return tmp[:-2]
-
-
-ydl_opts = {
-    "format": "bestaudio/best",
-    "writethumbnail": True,
-    "postprocessors": [
-        {
-            "key": "FFmpegExtractAudio",
-            "preferredcodec": "mp3",
-            "preferredquality": "192",
-        }
-    ],
-}
-
-
-def get_file_extension_from_url(url):
-    url_path = urlparse(url).path
-    basename = os.path.basename(url_path)
-    return basename.split(".")[-1]
 
 
 # Funtion To Download Song
@@ -244,29 +96,25 @@ async def download_song(url):
     return song_name
 
 
-is_downloading = False
+# Jiosaavn Music
 
 
-def time_to_seconds(time):
-    stringt = str(time)
-    return sum(int(x) * 60 ** i for i, x in enumerate(reversed(stringt.split(":"))))
-
-
-@app.on_message(filters.command("saavn") & ~filters.edited)
+@app.on_message(filters.command(["saavn", f"saavn@{BOT_USERNAME}"]) & ~filters.edited)
+@capture_err
 async def jssong(_, message):
     global is_downloading
     if len(message.command) < 2:
-        await message.reply_text("{},\n\nUse this format to download songs from saavn 👇\n\n<code>/saavn song_name</code>".format(message.from_user.mention))
+        await message.reply_text("Command `/saavn` requires an argument.")
         return
     if is_downloading:
         await message.reply_text(
-            "{},\n\nAnother download is in progress now ⏳\n\nPlease try again after 1 or 2 minutes 😊".format(message.from_user.mention)
+            "Sorry! **Another download is in progress !** Try Again After Sometime!"
         )
         return
     is_downloading = True
     text = message.text.split(None, 1)[1]
     query = text.replace(" ", "%20")
-    m = await message.reply_text("Now I am Searching Your Song 🔎\n\n@leosongdownloaderbot 🇱🇰")
+    m = await message.reply_text("**Please Wait! I'm Searching For Your Song 🔎...**")
     try:
         songs = await arq.saavn(query)
         if not songs.ok:
@@ -275,10 +123,14 @@ async def jssong(_, message):
         sname = songs.result[0].song
         slink = songs.result[0].media_url
         ssingers = songs.result[0].singers
-        await m.edit("Now I am Downloading Your Song ⏳\n\nPlease Wait 😊")
+        await m.edit("**Downloading Your Song! Please Wait ⏰**")
         song = await download_song(slink)
-        await m.edit("Now I am Uploading Your Song ⏳\n\nPlease Wait 😊")
-        await message.reply_audio(audio=song, title=sname, performer=ssingers)
+        await m.edit("**Uploading Your Song! Please Wait ⏰**")
+        await message.reply_audio(
+            audio=song,
+            title=sname,
+            performer=ssingers,
+        )
         os.remove(song)
         await m.delete()
     except Exception as e:

fb4b1b4dfb489637d4ebba61bbf1354e28056c32
matheeshaofficial <77558226+matheeshaofficial@users.noreply.github.com>
2021-07-31 19:49:51 +0530

    Update songs.py

--- LeoSongDownloaderBot/plugins/songs.py
+++ LeoSongDownloaderBot/plugins/songs.py
@@ -18,7 +18,7 @@ import aiofiles
 import aiohttp
 import requests
 import wget
-import youtube_dl
+from youtube_dl import YoutubeDL
 from pyrogram import Client, filters
 from pyrogram.errors import FloodWait, MessageNotModified
 from pyrogram.types import Message
